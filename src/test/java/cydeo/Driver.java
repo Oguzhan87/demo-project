14435565ffc584c3bfe528618fb28b048ebc57d3
Oguzhan87 <aydogan.oguzhan@hotmail.com>
2022-07-16 10:27:28 -0400

    trigger practice

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "52.90.101.317";
+                        String gridAddress = "44.202.17.69";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
@@ -102,6 +102,8 @@ public class Driver {
         }
 
         return driver;
+
+
     }
 
     public static void closeDriver() {
